d3671eead981d501d6b58135ed2c9fd9f0d3fb1f
Joao Pedro <joaopedrorabelodonascimento@gmail.com>
2026-09-14 21:41:56 -0300

    test: verifica cobertura de testes

--- velha.cpp
+++ velha.cpp
@@ -7,7 +7,7 @@
 
 /** 
  * @brief verifica situacao do jogo da velha  
- * @author Programador 
+ * @author João Pedro Rabelo do Nascimento 
  * @param  velha descreve o parametro
  * 
  *  Descrever o que a funcao faz
